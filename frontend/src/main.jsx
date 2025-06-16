8ee8e6ee89c91dc1656d5c7fa59ceafa5eb7e4ba
Shreyam <72690437+shreyam91@users.noreply.github.com>
2025-06-16 18:20:08 +0530

    Tryied multi language..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { LoadingProvider } from './context/LoadingContext.jsx'
+import './i18n';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
